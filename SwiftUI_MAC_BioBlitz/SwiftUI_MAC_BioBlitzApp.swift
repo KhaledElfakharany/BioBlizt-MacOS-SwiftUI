2832975f09fc20191bee1ec10940aab32b304246
Khaled Fakharany <khaled.fakharany@toptal.com>
2023-06-15 15:51:48 +0200

    Initial Commit

---
+++ SwiftUI_MAC_BioBlitz/SwiftUI_MAC_BioBlitzApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_MAC_BioBlitzApp.swift
+//  SwiftUI_MAC_BioBlitz
+//
+//  Created by Khaled Fakharany on 15/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_MAC_BioBlitzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
